5d68e38002021ae5c3096a55f52a52b31c41d66d
SavioUFC <saviocarvalho@alu.ufc.br>
2024-06-03 15:37:38 -0300

    fix: private route

--- src/main/java/com/example/authapi/infra/security/SecurityConfiguration.java
+++ src/main/java/com/example/authapi/infra/security/SecurityConfiguration.java
@@ -37,7 +37,7 @@ public class SecurityConfiguration{
                 .authorizeHttpRequests(authorizeHttpRequests -> authorizeHttpRequests
                         .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
                         .requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
-                        .requestMatchers(HttpMethod.POST, "/product").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.POST, "/product/add").hasRole("ADMIN")
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
